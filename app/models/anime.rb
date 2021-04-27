52cd6c23546f75a4cf14dfbfc074d8c1c94bf26a
matthewjstahnke <matthewjstahnke@gmail.com>
2021-04-26 22:13:21 -0500

    added anime to db

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,2 +1,11 @@
 class Anime < ApplicationRecord
+  def self.create_from_api(anime_data)
+    create(
+      title: anime_data["attributes"]["canonicalTitle"],
+      synopsis: anime_data["attributes"]["synopsis"],
+      date: anime_data["attributes"]["startDate"],
+      episodes: anime_data["attributes"]["episodeCount"],
+      poster: anime_data["attributes"]["posterImage"]["small"]
+    )
+  end
 end

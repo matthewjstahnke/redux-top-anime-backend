41aab566fadcf921cf22d6c7d53afb2add7d8610
matthewjstahnke <matthewjstahnke@gmail.com>
2021-04-27 19:05:55 -0500

    fixed route name

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post 'login', to: "sessions#login"
+  post 'autologin', to: "sessions#autologin"
   resources :users
   resources :animes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

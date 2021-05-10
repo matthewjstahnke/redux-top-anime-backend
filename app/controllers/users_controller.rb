0b21b244c57a8fb64eaa672b06f290465fcf35b6
matthewjstahnke <matthewjstahnke@gmail.com>
2021-05-09 22:34:25 -0500

    users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
   
   def index
     @users = User.all
 
+    render json: @users
+  end
+
+  def show
     render json: @users
   end
   
@@ -13,6 +18,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.permit(:username, :password)
